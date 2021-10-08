1549ea44f5ee3cf0e15d131c7a763bbd7f032c16
ChenXin-Ops <chenxinlin29@gmail.com>
2021-10-07 23:33:33 -0400

    triggering colors

--- controller and trigger/Assets/Triggering.cs
+++ controller and trigger/Assets/Triggering.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class Triggering : MonoBehaviour
 {
+    Color[] colors = new Color[6];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        colors[0] = Color.cyan;
+        colors[1] = Color.red;
+        colors[2] = Color.green;
+        colors[3] = new Color(255, 165, 0);
+        colors[4] = Color.yellow;
+        colors[5] = Color.magenta;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
     void OnTriggerEnter(Collider other) { 
         
         if (other.CompareTag("Player"))
         {
             Renderer render = GetComponent<Renderer>();
-        render.material.color = Color.red;
-        Debug.Log(other.gameObject.name);
+        render.material.color = colors[Random.Range(0, colors.Length)];
+
+            Debug.Log(other.gameObject.name);
         }
     }
 
